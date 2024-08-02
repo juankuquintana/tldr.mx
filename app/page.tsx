559cd4a363135bf1fa35c2e6cc0b39d3be222fd6
Juan Ku Quintana <juan@yellowme.mx>
2024-08-02 12:29:19 -0600

    Update copy

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,21 @@
 export default function Home() {
   return (
-    <main className="md:h-screen flex mx-6 mt-10 md:mt-0 md:items-center md:mx-12 lg:mx-24 2xl:mx-80">
+    <main className="md:h-screen flex mx-6 mt-10 mb-10 md:mb-0 md:mt-0 md:items-center md:mx-12 lg:mx-36 2xl:mx-80">
       <div className="w-screen flex flex-col md:flex-row md:justify-between">
         <div className="md:hidden mt-2 mb-8">——</div>
         <div>
           <h1 className="mb-2">&#128075; I&apos;m Juan</h1>
           <p className="mb-2">
-            I started as a software engineer at <a className="font-medium text-blue-600 underline hover:no-underline" href="https://www.nicmexico.mx/" target="_blank">nic.mx</a>
+            I enjoy working on Ruby on Rails, but I&apos;m fluent in ReactJS, NodeJS, Python...
           </p>
           <p className="mb-2">
-            Then, founded <a className="font-medium text-blue-600 underline hover:no-underline" href="https://nominal.mx/" target="_blank">nominal.mx</a>, an online invoicing platform
+            Former co-founder of <a className="font-medium text-blue-600 underline hover:no-underline" href="https://nominal.mx/" target="_blank">nominal.mx</a>, an invoicing platform
           </p>
           <p className="mb-2">
-            Later, I became a partner at <a className="font-medium text-blue-600 underline hover:no-underline" href="https://yellow.me/" target="_blank">Yellowme</a>, a product agency
-          </p>
-          <p>
-            Nowadays, I&apos;m a software engineer at <a className="font-medium text-blue-600 underline hover:no-underline" href="https://dnsimple.com/" target="_blank">DNSimple</a>
+            Former co-founder of <a className="font-medium text-blue-600 underline hover:no-underline" href="https://yellow.me/" target="_blank">Yellowme</a>, a product development company
           </p>
           <div className="flex pt-4 sm:mt-0">
-            <a href="https://www.linkedin.com/in/juankuquintana/" target="_blank" className="text-gray-500 hover:text-gray-900">
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
-              </svg>
-              <span className="sr-only">LinkedIn page</span>
-            </a>
-            <a href="https://twitter.com/juankuquintana" target="_blank" className="text-gray-500 hover:text-gray-900 ms-5">
+            <a href="https://twitter.com/juankuquintana" target="_blank" className="text-gray-500 hover:text-gray-900">
               <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 17">
                 <path fillRule="evenodd" d="M20 1.892a8.178 8.178 0 0 1-2.355.635 4.074 4.074 0 0 0 1.8-2.235 8.344 8.344 0 0 1-2.605.98A4.13 4.13 0 0 0 13.85 0a4.068 4.068 0 0 0-4.1 4.038 4 4 0 0 0 .105.919A11.705 11.705 0 0 1 1.4.734a4.006 4.006 0 0 0 1.268 5.392 4.165 4.165 0 0 1-1.859-.5v.05A4.057 4.057 0 0 0 4.1 9.635a4.19 4.19 0 0 1-1.856.07 4.108 4.108 0 0 0 3.831 2.807A8.36 8.36 0 0 1 0 14.184 11.732 11.732 0 0 0 6.291 16 11.502 11.502 0 0 0 17.964 4.5c0-.177 0-.35-.012-.523A8.143 8.143 0 0 0 20 1.892Z" clipRule="evenodd"/>
               </svg>
@@ -36,16 +27,25 @@ export default function Home() {
               </svg>
               <span className="sr-only">GitHub account</span>
             </a>
+            {/* <a href="https://www.linkedin.com/in/juankuquintana/" target="_blank" className="text-gray-500 hover:text-gray-900">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
+              </svg>
+              <span className="sr-only">LinkedIn page</span>
+            </a> */}
           </div>
         </div>
         <div className="mt-10 md:mt-0">
           <div className="mb-10 md:mb-15">
-            <h3 className="font-bold mb-2">Also working on...</h3>
+            <h3 className="font-bold mb-2">Building</h3>
             <p className="mb-2">
               <a className="font-medium text-blue-600 underline hover:no-underline" href="https://remotely.mx" target="_blank">remotely.mx</a>, a remote jobs board for 🇲🇽
             </p>
             <p className="mb-2">
-              <a className="font-medium text-blue-600 underline hover:no-underline" href="https://nominal.mx" target="_blank">nominal.mx</a>, a SaaS invoicing platform
+              <a className="font-medium text-blue-600 underline hover:no-underline" href="https://picks.games" target="_blank">picks.games</a>, a sports pools web app
+            </p>
+            <p className="mb-2">
+              Bringing back <a className="font-medium text-blue-600 underline hover:no-underline" href="https://nominal.mx" target="_blank">nominal.mx</a>
             </p>
           </div>
           <div>
@@ -57,7 +57,7 @@ export default function Home() {
               <a className="font-medium text-blue-600 underline hover:no-underline" href="https://signalvnoise.com/posts/1430-hire-managers-of-one" target="_blank">Managers of one</a> from <a className="font-medium text-blue-400 hover:no-underline" href="https://signalvnoise.com/" target="_blank">signalvnoise.com</a>
             </p>
             <p className="mb-2">
-              <a className="font-medium text-blue-600 underline hover:no-underline" href="https://hamvocke.com/blog/better-off-without-pull-requests/" target="_blank">Better without pull requests</a> from <a className="font-medium text-blue-400 hover:no-underline" href="https://hamvocke.com/" target="_blank">hamvocke.com</a>
+              <a className="font-medium text-blue-600 underline hover:no-underline" href="https://hamvocke.com/blog/better-off-without-pull-requests/" target="_blank">Better without PR&apos;s</a> from <a className="font-medium text-blue-400 hover:no-underline" href="https://hamvocke.com/" target="_blank">hamvocke.com</a>
             </p>
             <p className="mb-2">
               <a className="font-medium text-blue-600 underline hover:no-underline" href="https://hamvocke.com/blog/path-to-production/" target="_blank">Path to production</a> from <a className="font-medium text-blue-400 hover:no-underline" href="https://hamvocke.com/" target="_blank">hamvocke.com</a>
